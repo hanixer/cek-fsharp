3522e0ddd86261713c3eda2a914a0c53770c5b8a
Hanixer <jijer@jiji.com>
2020-01-11 12:29:46 +0200

    Changed data types for analysis

--- Anf.fs
+++ Anf.fs
@@ -3,43 +3,56 @@ module rec Anf
 /// let x = e1 e2 in e3
 /// x
 /// lambda x: e1
+/// number
+
+type TermId = int
 
 type Term =
-    | LetCall of name : string * func : string * arg : string * body : Term
-    | LetFunc of name : string * func : Lambda * body : Term
-    | Variable of name : string
+    | LetCall of name: string * func: string * arg: string * body: Term * id: TermId
+    | LetFunc of name: string * func: Lambda * body: Term * id: TermId
+    | Variable of name: string * id: TermId
+    | Int of int * id: TermId
 
 // Respresents argument and body of a function.
 type Lambda = string * Term
 
-// Maps from variable name to values.
-type Environment = Map<string, Value>
+/// Value address in store
+type ValAddress = string * TermId
+
+// Maps from variable name to values. Finite.
+type Environment = Map<string, ValAddress>
+
+/// Value store. Maps from value address to set of values
+type Store = Map<ValAddress, Set<Value>>
 
 // The only value for now is closure.
-type Value = Closure of Lambda * Environment
+type Value =
+    | Closure of Lambda * Environment
+    | Int of int
 
-// Continuation stack.
-type StackEntry = string * Term * Environment
+/// Address of continuation in continuation store.
+type ContAddress = TermId * Environment
 
-type Stack = list<StackEntry>
+/// Represents a call frame - to which variable the result must be bound,
+/// which expression should be evaulated next in which environment,
+/// and address of the next continuation.
+type Frame = string * Term * Environment * ContAddress
 
-type State = {
-    Term : Term
-    Env : Environment
-    Stack : Stack
-}
+/// Continuation store.
+/// Each continuation address can correspond to multiple return points.
+type ContStore = Map<ContAddress, Set<Frame>>
 
-/// Apply the value to the top of the continuation stack
-let continueWithVal value state =
-    match state.Stack with
-    | (var, term, env) :: rest ->
-        let env2 = Map.add var value env
-        [{Term = term; Env = env2; Stack = rest}]
-    | _ -> []
+type State =
+    { Term: Term
+      Env: Environment
+      Store: Store
+      ContStore: ContStore
+      ContAddress: ContAddress }
+
 
 let step state =
     match state.Term with
-    | Variable(name) ->
+    | Variable(name = name) ->
         let value = state.Env.[name]
         continueWithVal value state
     | LetCall(name, func, parameter, body) ->
@@ -47,7 +60,9 @@ let step state =
         let parameter' = state.Env.[parameter]
         let stackEntry = name, body, state.Env
         let funcEnv' = Map.add arg parameter' funcEnv
-        [{Term = funcBody; Env = funcEnv'; Stack = stackEntry :: state.Stack}]
+        [ { Term = funcBody
+            Env = funcEnv'
+            Stack = stackEntry :: state.Stack } ]
 
 let run term =
 
