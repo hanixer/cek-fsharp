a96ff66eef742bdb2106ff82a43d43a15c02f13d
Hanixer <jijer@jiji.com>
2020-01-11 20:35:06 +0200

    KCFA

--- Anf.fs
+++ Anf.fs
@@ -1,5 +1,8 @@
 module rec Anf
 
+/// K-CFA analysis for ANF language.
+/// Uses allocation scheme from paper "Pushdown Control-Flow Analysis for Free" by Gilray
+
 /// let x = e1 e2 in e3
 /// x
 /// lambda x: e1
@@ -8,8 +11,9 @@ module rec Anf
 type TermId = int
 
 type Term =
-    | LetCall of name: string * func: string * arg: string * body: Term * id: TermId
-    | LetFunc of name: string * func: Lambda * body: Term * id: TermId
+    | LetCall of name: string * func: Term * arg: Term * body: Term * id: TermId
+    | Let of name: string * value: Term * body: Term * id: TermId
+    | Lambda of func: Lambda * id: TermId
     | Variable of name: string * id: TermId
     | Int of int * id: TermId
 
@@ -49,21 +53,161 @@ type State =
       ContStore: ContStore
       ContAddress: ContAddress }
 
+let contAddressHalt = (-1, Map.empty)
+
+let getTermId term =
+    match term with
+    | Term.Int(id = id)
+    | Variable(id = id)
+    | Lambda(id = id)
+    | LetCall(id = id) -> id
+
+let addValueToStore addr value store =
+    match Map.tryFind addr store with
+    | Some(values) -> Map.add addr (Set.add value values) store
+    | _ -> Map.add addr (Set.singleton value) store
+
+let addManyToStore addr values store =
+    match Map.tryFind addr store with
+    | Some(values2) -> Map.add addr (Set.union values values2) store
+    | _ -> Map.add addr values store
+
+let returnValue termId value (state: State) =
+    if state.ContAddress = contAddressHalt then
+        Seq.empty
+    else
+        Seq.map (fun (frame: Frame) ->
+            let (retName, retTerm, retEnv, nextContAddr) = frame
+            let addr = (retName, termId)
+            let env = Map.add retName addr retEnv
+            let store = addValueToStore addr value state.Store
+            { Term = retTerm
+              Env = env
+              Store = store
+              ContStore = state.ContStore
+              ContAddress = nextContAddr }) state.ContStore.[state.ContAddress]
+
+let variableValue name state =
+    let addr = state.Env.[name]
+    state.Store.[addr]
+
+let unwrapLambda value =
+    match value with
+    | Closure((arg, body), env) -> arg, body, env
+    | _ -> failwith "closure is expected"
+
+let performCall name body termId funcVals argVals state =
+    seq {
+        for func in funcVals do
+            let arg, funcBody, funcEnv = unwrapLambda func
+            let addr = (arg, termId)
+            let env = Map.add arg addr funcEnv
+            let store = Map.add addr argVals state.Store
+            let contAddr = (getTermId funcBody, env)
+            let frame = (name, body, state.Env, state.ContAddress)
+            let contStore = addValueToStore contAddr frame state.ContStore
+            { Term = funcBody
+              Env = env
+              Store = store
+              ContStore = contStore
+              ContAddress = contAddr }
+    }
+
+let evaluateSimple term state =
+    match term with
+    | Variable(name, id) -> variableValue name state
+    | Term.Int(n, id) -> Set.singleton (Int(n))
+    | Lambda(func, id) -> Set.singleton (Closure(func, state.Env))
+    | _ -> failwith "simple expression expected"
 
 let step state =
     match state.Term with
-    | Variable(name = name) ->
-        let value = state.Env.[name]
-        continueWithVal value state
-    | LetCall(name, func, parameter, body) ->
-        let (Closure((arg, funcBody), funcEnv)) = state.Env.[func]
-        let parameter' = state.Env.[parameter]
-        let stackEntry = name, body, state.Env
-        let funcEnv' = Map.add arg parameter' funcEnv
-        [ { Term = funcBody
-            Env = funcEnv'
-            Stack = stackEntry :: state.Stack } ]
+    | Variable(name, id) ->
+        let values = variableValue name state
+        Seq.collect (fun value -> returnValue id value state) values
+    | Term.Int(n, id) ->
+        let value = Int(n)
+        returnValue id value state
+    | Lambda(func, id) ->
+        let value = Closure(func, state.Env)
+        returnValue id value state
+    | Let(name, rhs, body, termId) ->
+        let values = evaluateSimple rhs state
+        let addr = (name, termId)
+        let env = Map.add name addr state.Env
+        let store = addManyToStore addr values state.Store
+
+        let newState =
+            { state with
+                  Term = body
+                  Env = env
+                  Store = store }
+
+        Seq.singleton newState
+    | LetCall(name, func, arg, body, termId) ->
+        let funcVals = evaluateSimple func state
+        let argVals = evaluateSimple arg state
+        performCall name body termId funcVals argVals state
+
+let inject term =
+    { Term = term
+      Env = Map.empty
+      Store = Map.empty
+      ContStore = Map.empty
+      ContAddress = contAddressHalt }
 
 let run term =
+    let state = inject term
 
-    term
+    let rec loop seen todo =
+        if Seq.isEmpty todo then
+            seen
+        else
+            let state = Seq.head todo
+            let rest = Seq.tail todo
+            if Set.contains state seen then
+                loop seen rest
+            else
+                printfn "%A\n----------------" state
+                let newStates = step state
+                let seen = Set.add state seen
+                let todo = Seq.append newStates rest
+                loop seen todo
+
+    loop Set.empty (Seq.singleton state)
+
+//////////////////////////////////
+/// Examples
+
+/// Works for this example
+/// a -> {1}
+/// b -> {2}
+
+/// (let ((id (lambda (x) x)))
+///     (let ((a (id 1)))
+///         (let ((b (id 2)))
+///             b)))
+let term1 =
+    Let("id", Lambda(("x", Variable("x", 0)), 1),
+      LetCall("a", Variable("id", 2), Term.Int(1, 3),
+        LetCall("b", Variable("id", 4), Term.Int(2, 5),
+            Variable("b", 6), 7), 8), 9)
+
+/// Fails for this example
+/// a -> {1, 2}
+/// b -> {1, 2}
+
+/// (let ((id (lambda (x) x)))
+///     (let ((call-id (lambda (y) y)))
+///         (let ((a (call-id 1)))
+///             (let ((b (call-id 2)))
+///                 b))))
+let term2 =
+    Let("id", Lambda(("x", Variable("x", 0)), 1),
+      Let("call-id",
+          Lambda(("y",
+                  LetCall("z", Variable("id", 2), Variable("y", 3),
+                          Variable("z", 4), 5)), 6),
+            LetCall("a", Variable("call-id", 7), Term.Int(1, 8),
+              LetCall("b", Variable("call-id", 9), Term.Int(2, 10),
+                Variable("b", 11), 12), 13), 14), 15)
--- Program.fs
+++ Program.fs
@@ -1,108 +1,107 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open Cps
-open KCfa
-open SExpr
+open Anf
 
-let program : Program =
-    123, ["k"], Call(1, Simple.PrimitiveOp(4, Add), [Simple.Int(2, 1); Simple.Int(3, 2); Reference(123, 55, "k")])
+module Example =
+    open Cps
+    open KCfa
+    open SExpr
 
-// 1 => (lambda (k) (k 1))
-// (+ 1 2) => (lambda (k) (+ 1 2 k))
-// (+ (+ 1 2) (+ 3 4)) => (+ 1 2 (lambda (a) (+ 3 4 (lambda (b) (+ a b)))))
+    let program : Program =
+        123, ["k"], Call(1, Simple.PrimitiveOp(4, Add), [Simple.Int(2, 1); Simple.Int(3, 2); Reference(123, 55, "k")])
 
-let toCps sexpr =
-    let mutable counter = 0
-    let freshLabel() =
-        counter <- counter + 1
-        counter
+    // 1 => (lambda (k) (k 1))
+    // (+ 1 2) => (lambda (k) (+ 1 2 k))
+    // (+ (+ 1 2) (+ 3 4)) => (+ 1 2 (lambda (a) (+ 3 4 (lambda (b) (+ a b)))))
 
-    let bindingToName = function
-        | List [Symbol name; _] -> name
-        | _ -> failwith "wrong binding"
+    let toCps sexpr =
+        let mutable counter = 0
+        let freshLabel() =
+            counter <- counter + 1
+            counter
 
-    let extendEnv names label env =
-        List.fold (fun env name -> Map.add name label env) env names
+        let bindingToName = function
+            | List [Symbol name; _] -> name
+            | _ -> failwith "wrong binding"
 
-    let rec parseComplex env sexpr =
-        match sexpr with
-        | List [Symbol "letrec"; List bindings; body] ->
-            let label = freshLabel()
-            let names = List.map bindingToName bindings
-            let env2 = extendEnv names label env
-            let bindings = parseBindings env2 bindings
-            let body = parseComplex env2 body
-            Letrec(label, bindings, body)
-        | List(func :: args) ->
-            let funcE = parseSimple env func
-            let argsE = parseSimpleMany env args
-            Call(freshLabel(), funcE, argsE)
-        | _ -> failwith "wrong complex expression"
+        let extendEnv names label env =
+            List.fold (fun env name -> Map.add name label env) env names
 
-    and parseBindings env bindings =
-        List.map (fun binding ->
-            match binding with
-            | List [Symbol name; rhs] -> (name, parseSimple env rhs)
-            | _ -> failwith "wrong binding 2") bindings
+        let rec parseComplex env sexpr =
+            match sexpr with
+            | List [Symbol "letrec"; List bindings; body] ->
+                let label = freshLabel()
+                let names = List.map bindingToName bindings
+                let env2 = extendEnv names label env
+                let bindings = parseBindings env2 bindings
+                let body = parseComplex env2 body
+                Letrec(label, bindings, body)
+            | List(func :: args) ->
+                let funcE = parseSimple env func
+                let argsE = parseSimpleMany env args
+                Call(freshLabel(), funcE, argsE)
+            | _ -> failwith "wrong complex expression"
 
-    and parseSimple env sexpr =
-        match sexpr with
-        | Symbol "+" -> Simple.PrimitiveOp(freshLabel(), Add)
-        | Symbol "if" -> Simple.PrimitiveOp(freshLabel(), If)
-        | Symbol "zero?" -> Simple.PrimitiveOp(freshLabel(), IsZero)
-        | Symbol variable ->
-            let binder = Map.find variable env
-            let label = freshLabel()
-            Reference(binder, label, variable)
-        | SExpr.Number(n) -> Simple.Int(freshLabel(), n)
-        | SExpr.Bool(b) -> Simple.Bool(freshLabel(), b)
-        | List [Symbol "lambda"; List args; body] ->
-            let label = freshLabel()
-            let args = symbolsToStrings args
-            let env2 = extendEnv args label env
-            let body = parseComplex env2 body
-            Lambda(label, args, body)
-        | _ -> failwith "wrong simple expr"
+        and parseBindings env bindings =
+            List.map (fun binding ->
+                match binding with
+                | List [Symbol name; rhs] -> (name, parseSimple env rhs)
+                | _ -> failwith "wrong binding 2") bindings
 
-    and parseSimpleMany env sexprs = List.map (parseSimple env) sexprs
+        and parseSimple env sexpr =
+            match sexpr with
+            | Symbol "+" -> Simple.PrimitiveOp(freshLabel(), Add)
+            | Symbol "if" -> Simple.PrimitiveOp(freshLabel(), If)
+            | Symbol "zero?" -> Simple.PrimitiveOp(freshLabel(), IsZero)
+            | Symbol variable ->
+                let binder = Map.find variable env
+                let label = freshLabel()
+                Reference(binder, label, variable)
+            | SExpr.Number(n) -> Simple.Int(freshLabel(), n)
+            | SExpr.Bool(b) -> Simple.Bool(freshLabel(), b)
+            | List [Symbol "lambda"; List args; body] ->
+                let label = freshLabel()
+                let args = symbolsToStrings args
+                let env2 = extendEnv args label env
+                let body = parseComplex env2 body
+                Lambda(label, args, body)
+            | _ -> failwith "wrong simple expr"
 
-    match parseSimple Map.empty sexpr with
-    | Lambda(lambda) -> lambda
-    | _ -> failwith "lambda expected on top level"
+        and parseSimpleMany env sexprs = List.map (parseSimple env) sexprs
 
-// (lambda (stop)
-//   (lambda (x y k) (+ x y k)) 1 2 stop)
-// (lambda (stop) (letrec ((f (lambda (n k) (zero? n (lambda (z) (if z (lambda () 0) (lambda () (+ n -1) (lambda (m) (f m (lambda (fm) (+ n fm (lambda (plu) (k plu))))))))))))) (f 5 stop))
-let s1 = "(lambda (stop) ((lambda (x y k) (+ x y k)) 1 2 stop))"
-let s2 = "(lambda (stop) (letrec ((f (lambda (n k) (zero? n (lambda (z) (if z (lambda () (k 0)) (lambda () (+ n -1 (lambda (m) (f m (lambda (fm) (+ n fm k)))))))))))) (f 5 stop)))"
+        match parseSimple Map.empty sexpr with
+        | Lambda(lambda) -> lambda
+        | _ -> failwith "lambda expected on top level"
 
-// (let* ((id (lambda (x) x))
-//        (a  (id (lambda (z) (stop z))))
-//        (b  (id (lambda (y) (stop y)))))
-//   (stop b))
-let s3 = "
-(lambda (stop)
-  ((lambda (id)
+    // (lambda (stop)
+    //   (lambda (x y k) (+ x y k)) 1 2 stop)
+    // (lambda (stop) (letrec ((f (lambda (n k) (zero? n (lambda (z) (if z (lambda () 0) (lambda () (+ n -1) (lambda (m) (f m (lambda (fm) (+ n fm (lambda (plu) (k plu))))))))))))) (f 5 stop))
+    let s1 = "(lambda (stop) ((lambda (x y k) (+ x y k)) 1 2 stop))"
+    let s2 = "(lambda (stop) (letrec ((f (lambda (n k) (zero? n (lambda (z) (if z (lambda () (k 0)) (lambda () (+ n -1 (lambda (m) (f m (lambda (fm) (+ n fm k)))))))))))) (f 5 stop)))"
 
-    (id (lambda (z) (stop z)) (lambda (a)
+    // (let* ((id (lambda (x) x))
+    //        (a  (id (lambda (z) (stop z))))
+    //        (b  (id (lambda (y) (stop y)))))
+    //   (stop b))
+    let s3 = "
+    (lambda (stop)
+      ((lambda (id)
 
-      (id (lambda (y) (stop y)) (lambda (b)
-        (stop b)
-      ))
-    ))
-    )
+        (id (lambda (z) (stop z)) (lambda (a)
 
-   (lambda (x k) (k x)))
-  )"
+          (id (lambda (y) (stop y)) (lambda (b)
+            (stop b)
+          ))
+        ))
+        )
+
+       (lambda (x k) (k x)))
+      )"
 
 
 [<EntryPoint>]
 let main argv =
-
-    let program = (toCps (stringToSExpr s3))
-    printfn "program:\n %A\n" program
-    ExactCfa.runProgram program
-
+    Anf.run Anf.term2
 
     0 // return an integer exit code
